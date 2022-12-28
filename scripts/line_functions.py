628ab48ed4d71ea757ac8b02d206b33993536e64
NamWoo <pre3ice@gmail.com>
2022-12-28 11:25:06 +0900

    add bass houghLinesP

--- scripts/line_functions.py
+++ scripts/line_functions.py
@@ -1,5 +1,28 @@
 
-##from https://github.com/NamWoo
+import cv2
+import numpy as np
+from line_utils import *
+
+h_l = 0
+s_l = 100
+v_l = 120
+h_u = 25
+s_u = 255
+v_u = 160
+
+lower = np.array([h_l, s_l, v_l])
+upper = np.array([h_u, s_u, v_u])
 
 def line_dectecting(cv_image):
+    
+    cv_image = convertColor(cv_image, cv2.COLOR_BGR2HSV)
+    cv_image = splitColor(cv_image, lower, upper)
+    cv_image = convertColor(cv_image, cv2.COLOR_HSV2BGR)
+    
+    cv_image = cannyEdge(cv_image, 100, 200)
+    
+    lines = houghLinesP(cv_image, 1, np.pi/180, 100, 10, 50)
+    cv_image = drawHoughLinesP(cv_image, lines)
+
+
     return cv_image
--- scripts/line_utils.py
+++ scripts/line_utils.py
@@ -27,3 +27,30 @@ def drawLine(image, point1, point2, color=(255, 0, 0), thickness=3, lineType=cv2
 def drawCircle(image, center, radius, color=(255, 0, 0), thickness=3, lineType=cv2.LINE_AA):
     result = imageCopy(image)
     return cv2.circle(result, center, radius, color, thickness, lineType)
+
+def splitColor(image, lower, upper):
+    result = imageCopy(image)
+    mask = rangeColor(result, lower, upper)
+    return cv2.bitwise_and(result, result, mask=mask)
+
+def convertColor(image, flag=cv2.COLOR_BGR2GRAY):
+    return cv2.cvtColor(image, flag)
+
+def rangeColor(image, lower, upper):
+    result = imageCopy(image)
+    return cv2.inRange(result, lower, upper)
+
+def cannyEdge(image, threshold1=100, threshold2=200):
+    return cv2.Canny(image, threshold1, threshold2) 
+
+def houghLinesP(image, rho=1.0, theta=np.pi/180, threshold=100, minLineLength=10, maxLineGap=100):
+    return cv2.HoughLinesP(image, rho, theta, threshold, minLineLength=minLineLength, maxLineGap=maxLineGap)
+
+def drawHoughLinesP(image, lines):
+    result = imageCopy(image)
+    if len(image.shape) == 2:
+        result = convertColor(image, cv2.COLOR_GRAY2BGR)
+    for i in range(len(lines)):
+        for x1, y1, x2, y2 in lines[i]:
+            cv2.line(result, (x1, y1), (x2, y2), (0, 0, 255), 3)
+    return result
